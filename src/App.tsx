dd73d0045169ecc3a58a438c731aa6612b159c02
Siwat Anandechsak <bright.siwat@hotmail.com>
2023-10-02 14:56:49 +0700

    demo: event handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,42 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import Greeting from './components/Greeting'
 import Navbar from './components/Navbar'
+import Post from './components/Post'
+import { PostDTO } from './types/dto'
+import { postsData } from './data'
 
-const name: string = 'Bright'
-const greetingMsg: string = 'Hello'
 function App() {
+  const getPosts = (): Promise<PostDTO[]> => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(postsData)
+      }, 2 * 1000)
+    })
+  }
+
+  const name: string = 'Bright'
+  const greetingMsg: string = 'Hello'
+  const isLoggedIn: boolean = true
+  const [loading, setLoading] = useState(false)
+  const [posts, setPosts] = useState<PostDTO[]>([])
+  useEffect(() => {
+    getPosts().then((data) => {
+      setLoading(true)
+      setPosts(data)
+    })
+  }, [])
   return (
     <div className="App">
       <Navbar />
-      <Greeting name={name} greetingMsg={greetingMsg} isLoggedIn={false} />
+      <Greeting name={name} greetingMsg={greetingMsg} isLoggedIn={isLoggedIn} />
+      <div className="feed-container">
+        {loading
+          ? posts.map((post) => {
+              return <Post key={post.id} post={post} />
+            })
+          : 'Loading...'}
+      </div>
     </div>
   )
 }
---
+++ src/data.ts
@@ -0,0 +1,22 @@
+import { PostDTO } from './types/dto'
+
+export const postsData: PostDTO[] = [
+  {
+    id: 1,
+    userId: 1,
+    title: "Let's learn React!",
+    body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
+  },
+  {
+    id: 2,
+    userId: 2,
+    title: 'How to install Node.js',
+    body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
+  },
+  {
+    id: 3,
+    userId: 3,
+    title: 'Basic HTML',
+    body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
+  },
+]
