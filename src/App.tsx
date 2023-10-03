cf4bbdc338a07ca271aa795a8fdccedf0da576d3
Siwat Anandechsak <bright.siwat@hotmail.com>
2023-10-03 14:46:54 +0700

    clear form

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { FormEvent, useEffect, useState } from 'react'
 import './App.css'
 import Greeting from './components/Greeting'
 import Navbar from './components/Navbar'
@@ -7,6 +7,14 @@ import { PostDTO } from './types/dto'
 import { postsData } from './data'
 
 function App() {
+  const [loading, setLoading] = useState(false)
+  const [posts, setPosts] = useState<PostDTO[]>([])
+  const [newTitle, setNewTitle] = useState<string>('')
+  const [newBody, setNewBody] = useState<string>('')
+
+  const name: string = 'Bright'
+  const isLoggedIn: boolean = true
+
   const getPosts = (): Promise<PostDTO[]> => {
     return new Promise((resolve) => {
       setTimeout(() => {
@@ -15,20 +23,42 @@ function App() {
     })
   }
 
-  const name: string = 'Bright'
-  const isLoggedIn: boolean = true
-  const [loading, setLoading] = useState(false)
-  const [posts, setPosts] = useState<PostDTO[]>([])
   useEffect(() => {
     getPosts().then((data) => {
       setLoading(true)
       setPosts(data)
     })
   }, [])
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault()
+    const currentPosts = [...posts]
+    currentPosts.push({
+      id: Math.floor(Math.random() * 1000),
+      userId: Math.floor(Math.random() * 1000),
+      title: newTitle,
+      body: newBody,
+    })
+    setPosts(currentPosts)
+
+    setNewTitle('')
+    setNewBody('')
+  }
+
+  const handleNewTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => setNewTitle(e.target.value)
+  const handleNewBodyChange = (e: React.ChangeEvent<HTMLInputElement>) => setNewBody(e.target.value)
+
   return (
     <div className="App">
       <Navbar />
       <Greeting name={name} isLoggedIn={isLoggedIn} />
+      <form onSubmit={handleSubmit}>
+        <label>Title</label>
+        <input type="text" value={newTitle} required onChange={handleNewTitleChange} />
+        <label>Body</label>
+        <input type="text" value={newBody} required onChange={handleNewBodyChange} />
+        <button type="submit">submit</button>
+      </form>
       <div className="feed-container">
         {loading
           ? posts.map((post) => {
